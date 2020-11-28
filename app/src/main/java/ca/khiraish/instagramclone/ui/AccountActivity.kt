ddea3be6a1d126eeca89411b04151b33050d06ca
khishh <khiraishi0319@gmail.com>
2020-11-28 23:42:35 +0900

    added gradle dependencies and plug ins

--- app/src/main/java/ca/khiraish/instagramclone/ui/AccountActivity.kt
+++ app/src/main/java/ca/khiraish/instagramclone/ui/AccountActivity.kt
@@ -1,11 +1,12 @@
-package ca.khiraish.instagramclone
+package ca.khiraish.instagramclone.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ca.khiraish.instagramclone.R
 
-class MainActivity : AppCompatActivity() {
+class AccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_account)
     }
 }
---
+++ app/src/main/java/ca/khiraish/instagramclone/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ca.khiraish.instagramclone.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ca.khiraish.instagramclone.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
